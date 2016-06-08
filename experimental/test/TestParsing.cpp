c9ab9f6cc3af13d6bef0f8fec29583d2444da86f
Karl Schimpf <kschimpf@google.com>
2016-06-08 12:11:15 -0700

    Save experimental coding.

---
+++ experimental/test/TestParsing.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "filter_parser.tab.hpp"
+#include "FilterDriver.h"
+
+int main(int argc, char *argv[]) {
+  int res = 0;
+  wasm::filt::FilterDriver driver;
+  for (int i = 1; i < argc; ++i)
+    if (argv[i] == std::string ("-p"))
+      driver.trace_parsing = true;
+    else if (argv[i] == std::string ("-s"))
+      driver.trace_scanning = true;
+    else if (!driver.parse (argv[i]))
+      std::cout << driver.result << std::endl;
+    else
+      res = 1;
+  return res;
+}
