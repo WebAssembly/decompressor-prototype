d852f789c6f29c4c2ca907781b5ce9034586a663
KarlSchimpf <karlschimpf@gmail.com>
2016-08-06 09:43:31 -0700

    Detemplatize class Queue. (#160)

--- src/stream/ByteQueue.cpp
+++ src/stream/ByteQueue.cpp
@@ -1,29 +1,26 @@
-/* -*- C++ -*- */
-/*
- * Copyright 2016 WebAssembly Community Group participants
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// -*- C++ -*- */
+//
+// Copyright 2016 WebAssembly Community Group participants
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
 #include "stream/ByteQueue.h"
-#include "stream/QueueImpl.h"
 #include "stream/WriteUtils.h"
 
 namespace wasm {
 
 namespace decode {
-
-template class Queue<uint8_t>;
 
 void ByteQueue::writePageAt(FILE* File, size_t Address) {
   std::shared_ptr<Page> P = getPage(Address);
--- src/stream/Queue.cpp
+++ src/stream/Queue.cpp
@@ -1,23 +1,19 @@
-/* -*- C++ -*- */
-/*
- * Copyright 2016 WebAssembly Community Group participants
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// Implements a queue for hold buffers to streams.
-#ifndef DECOMPRESSOR_SRC_STREAM_QUEUEIMPL_H
-#define DECOMPRESSOR_SRC_STREAM_QUEUEIMPL_H
+// -*- C++ -*-
+//
+// Copyright 2016 WebAssembly Community Group participants
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+///
 
 #include "stream/Queue.h"
 
@@ -25,43 +21,35 @@ namespace wasm {
 
 namespace decode {
 
-template <class Base>
-Queue<Base>::Queue()
-    : MinPeekSize(32 * sizeof(Base)),
+Queue::Queue()
+    : MinPeekSize(32),
       EobFrozen(false),
       EofPtr(std::make_shared<BlockEob>()) {
   LastPage = FirstPage = std::make_shared<Page>(0);
   PageMap.push_back(LastPage);
-  // Double check that we can evenly fit elements of Base in a page.
-  assert(Page::Size % sizeof(Base) == 0);
 }
 
-template <class Base>
-Queue<Base>::~Queue() {
+Queue::~Queue() {
   while (FirstPage)
     dumpFirstPage();
 }
 
-template <class Base>
-void Queue<Base>::dumpFirstPage() {
+void Queue::dumpFirstPage() {
   FirstPage = FirstPage->Next;
 }
 
-template <class Base>
-void Queue<Base>::dumpPreviousPages() {
+void Queue::dumpPreviousPages() {
   while (FirstPage.unique())
     dumpFirstPage();
 }
 
-template <class Base>
-bool Queue<Base>::readFill(size_t Address) {
+bool Queue::readFill(size_t Address) {
   return Address < LastPage->getMaxAddress();
 }
 
-template <class Base>
-size_t Queue<Base>::readFromPage(size_t Address,
-                                 size_t WantedSize,
-                                 PageCursor& Cursor) {
+size_t Queue::readFromPage(size_t Address,
+                           size_t WantedSize,
+                           PageCursor& Cursor) {
   // Start by read-filling if necessary.
   if (Address >= LastPage->getMaxAddress()) {
     if (!readFill(Address))
@@ -74,16 +62,14 @@ size_t Queue<Base>::readFromPage(size_t Address,
     return 0;
   dumpPreviousPages();
   // Compute largest contiguous range of elements available.
-  WantedSize *= sizeof(Base);
   if (Address + WantedSize > Cursor.getMaxAddress())
-    WantedSize = (Cursor.getMaxAddress() - Address) / sizeof(Base);
+    WantedSize = Cursor.getMaxAddress() - Address;
   return WantedSize;
 }
 
-template <class Base>
-size_t Queue<Base>::writeToPage(size_t Address,
-                                size_t WantedSize,
-                                PageCursor& Cursor) {
+size_t Queue::writeToPage(size_t Address,
+                          size_t WantedSize,
+                          PageCursor& Cursor) {
   // Expand till page exists.
   while (Address >= LastPage->getMaxAddress()) {
     if (EobFrozen)
@@ -103,15 +89,12 @@ size_t Queue<Base>::writeToPage(size_t Address,
     return 0;
   dumpPreviousPages();
   // Compute largest contiguous range of bytes available.
-  WantedSize *= sizeof(Base);
-  if (Address + WantedSize > Cursor.getMaxAddress()) {
-    WantedSize = (Cursor.getMaxAddress() - Address) / sizeof(Base);
-  }
+  if (Address + WantedSize > Cursor.getMaxAddress())
+    WantedSize = Cursor.getMaxAddress() - Address;
   return WantedSize;
 }
 
-template <class Base>
-void Queue<Base>::freezeEof(size_t Address) {
+void Queue::freezeEof(size_t Address) {
   assert(Address != kUndefinedAddress && "WASM stream too big to process");
   assert(!EobFrozen);
   // This call zero-fill pages if writing hasn't reached Address yet.
@@ -129,5 +112,3 @@ void Queue<Base>::freezeEof(size_t Address) {
 }  // end of decode namespace
 
 }  // end of wasm namespace
-
-#endif  // DECOMPRESSOR_SRC_STREAM_QUEUEIMPL_H
