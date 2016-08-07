3fafc4da585cad34f61a1289d4c0b8e8ccfc5862
KarlSchimpf <karlschimpf@gmail.com>
2016-08-06 17:35:46 -0700

    Align to byte after section blocks. (#169)

--- src/interp/Interpreter.cpp
+++ src/interp/Interpreter.cpp
@@ -1,19 +1,18 @@
-/* -*- C++ -*- */
-/*
- * Copyright 2016 WebAssembly Community Group participants
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// -*- C++ -*- */
+//
+// Copyright 2016 WebAssembly Community Group participants
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // Implements the interpreter for filter s-expressions.
 
@@ -552,6 +551,8 @@ void Interpreter::decompressSection() {
   Trace.traceString("name", CurSectionName);
   SymbolNode* Sym = Symtab->getSymbol(CurSectionName);
   decompressBlock(Sym ? Sym->getDefineDefinition() : nullptr);
+  Reader->alignToByte(ReadPos);
+  Writer->alignToByte(WritePos);
 }
 
 void Interpreter::readSectionName() {
