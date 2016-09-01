8f9069c27d8524cfb0100558064136bad75299ac
KarlSchimpf <karlschimpf@gmail.com>
2016-09-01 15:29:13 -0700

    Clean up call/returns in state machine for decompression s-expressions. (#217)
    * At eof, check that input queue didn't cause errors.
    
    * Clean up notion of call/return for reading s-expressions.

--- src/interp/Interpreter.cpp
+++ src/interp/Interpreter.cpp
@@ -121,8 +121,8 @@ Interpreter::Interpreter(std::shared_ptr<Queue> Input,
 }
 
 void Interpreter::CallFrame::describe(FILE* File, TextWriter* Writer) const {
-  fprintf(File, "%s.%s = %" PRIuMAX ": ", MethodName[int(CallMethod)],
-          StateName[int(CallState)], uintmax_t(ReturnValue));
+  fprintf(File, "%s.%s = %" PRIuMAX ": ", getName(CallMethod),
+          getName(CallState), uintmax_t(ReturnValue));
   if (Nd)
     Writer->writeAbbrev(File, Nd);
   else
@@ -855,7 +855,7 @@ void Interpreter::resume() {
           case State::Failed:
             break;
           default:
-            TRACE(string, "State", StateName[int(Frame.CallState)]);
+            TRACE(string, "State", getName(Frame.CallState));
             TRACE_MESSAGE("Malformed finish state found, Correcting!");
             Frame.CallState = State::Failed;
         }
@@ -1295,10 +1295,10 @@ void Interpreter::resume() {
         // If reached, we finished processing the input.
         assert(FrameStack.empty());
         Frame.CallMethod = Method::Finished;
-        if (ReadPos.atEof())
+        if (ReadPos.atEof() && ReadPos.isQueueGood())
           Frame.CallState = State::Succeeded;
         else
-          fail("Extraneous bytes found at the end of the input stream");
+          fail("Malformed input in compressed file");
         break;
       case Method::Write:
         switch (Frame.Nd->getType()) {
