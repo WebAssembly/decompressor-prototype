add05f344b4372c553b2b859b7876bae041a5199
Karl Schimpf <kschimpf@google.com>
2016-12-06 15:37:28 -0800

    Define a trie walker for count nodes.

--- src/intcomp/IntCountNode.cpp
+++ src/intcomp/IntCountNode.cpp
@@ -142,6 +142,13 @@ RootCountNode::RootCountNode()
 RootCountNode::~RootCountNode() {
 }
 
+void RootCountNode::getOthers(OthersList& L) const {
+  L.push_back(BlockEnter);
+  L.push_back(BlockExit);
+  L.push_back(DefaultSingle);
+  L.push_back(DefaultMultiple);
+}
+
 void RootCountNode::describe(FILE* Out, size_t NestLevel) const {
   indent(Out, NestLevel);
   fputs(": Root", Out);
@@ -291,6 +298,122 @@ void IntSeqCountNode::describeValues(FILE* Out) const {
   }
   if (ElidedCount)
     fprintf(Out, " ...[%" PRIuMAX "]", ElidedCount);
+}
+
+namespace {
+
+const char* StateName[]{"enter", "visiting", "Exit"};
+
+}  // end of anonymous namespace
+
+const char* CountNodeVisitor::getName(State St) {
+  return StateName[unsigned(St)];
+}
+
+CountNodeVisitor::Frame::~Frame() {
+}
+
+void CountNodeVisitor::Frame::describe(FILE* Out) const {
+  describePrefix(Out);
+  fprintf(stderr, "  %" PRIuMAX "..%" PRIuMAX " [%" PRIuMAX "] %s\n",
+          uintmax_t(FirstKid), uintmax_t(LastKid), uintmax_t(CurKid),
+          getName(CurState));
+  getNode()->describe(Out);
+  describeSuffix(Out);
+}
+
+void CountNodeVisitor::Frame::describePrefix(FILE* Out) const {
+  fputs("<frame", Out);
+}
+
+void CountNodeVisitor::Frame::describeSuffix(FILE* Out) const {
+  fputs(">\n", Out);
+}
+
+CountNodeVisitor::CountNodeVisitor(CountNode::RootPtr Root) : Root(Root) {
+}
+
+CountNodeVisitor::FramePtr CountNodeVisitor::getFrame(size_t FirstKid,
+                                                      size_t LastKid) {
+  return std::make_shared<Frame>(*this, FirstKid, LastKid);
+}
+
+CountNodeVisitor::FramePtr CountNodeVisitor::getFrame(CountNode::IntPtr Nd,
+                                                      size_t FirstKid,
+                                                      size_t LastKid) {
+  return std::make_shared<Frame>(*this, Nd, FirstKid, LastKid);
+}
+
+void CountNodeVisitor::describe(FILE* Out) const {
+  fprintf(stderr, "*** Stack ***\n");
+  for (auto Frame : Stack) {
+    Frame->describe(Out);
+  }
+  fprintf(stderr, "*************\n");
+}
+
+void CountNodeVisitor::walk() {
+  callRoot();
+  walkOther();
+  while (!Stack.empty()) {
+    FramePtr Frame = Stack.back();
+    switch (Frame->CurState) {
+      case State::Enter: {
+        if (Frame->CurKid >= Frame->LastKid) {
+          Frame->CurState = State::Visiting;
+          break;
+        }
+        callNode(ToVisit[Frame->CurKid++]);
+        break;
+      }
+      case State::Visiting: {
+        Frame->CurState = State::Exit;
+        visit(Frame);
+        break;
+      }
+      case State::Exit: {
+        while (ToVisit.size() > Frame->FirstKid)
+          ToVisit.pop_back();
+        Stack.pop_back();
+        visitReturn(Frame);
+        break;
+      }
+    }
+  }
+}
+
+void CountNodeVisitor::walkOther() {
+  CountNode::OthersList L;
+  Root->getOthers(L);
+  for (CountNode::Ptr Nd : L)
+    visitOther(Nd);
+}
+
+void CountNodeVisitor::callRoot() {
+  size_t FirstKid = ToVisit.size();
+  for (CountNode::SuccMapIterator Iter = Root->getSuccBegin(),
+                                  End = Root->getSuccEnd();
+       Iter != End; ++Iter)
+    ToVisit.push_back(Iter->second);
+  Stack.push_back(getFrame(FirstKid, ToVisit.size()));
+}
+
+void CountNodeVisitor::callNode(CountNode::IntPtr Nd) {
+  size_t FirstKid = ToVisit.size();
+  for (CountNode::SuccMapIterator Iter = Nd->getSuccBegin(),
+                                  End = Nd->getSuccEnd();
+       Iter != End; ++Iter)
+    ToVisit.push_back(Iter->second);
+  Stack.push_back(getFrame(Nd, FirstKid, ToVisit.size()));
+}
+
+void CountNodeVisitor::visit(FramePtr Frame) {
+}
+
+void CountNodeVisitor::visitReturn(FramePtr Frame) {
+}
+
+void CountNodeVisitor::visitOther(CountNode::Ptr Nd) {
 }
 
 }  // end of namespace intcomp
--- src/intcomp/IntCountNode.h
+++ src/intcomp/IntCountNode.h
@@ -57,6 +57,7 @@ class CountNode : public std::enable_shared_from_this<CountNode> {
   typedef std::shared_ptr<RootCountNode> RootPtr;
   typedef std::shared_ptr<CountNodeWithSuccs> WithSuccsPtr;
   typedef std::map<decode::IntType, CountNode::IntPtr> SuccMap;
+  typedef std::vector<Ptr> OthersList;
   typedef SuccMap::const_iterator SuccMapIterator;
   typedef Ptr HeapValueType;
   typedef utils::heap<HeapValueType> HeapType;
@@ -213,6 +214,8 @@ class RootCountNode : public CountNodeWithSuccs {
   void describe(FILE* Out, size_t NestLevel = 0) const OVERRIDE;
   int compare(const CountNode& Nd) const OVERRIDE;
 
+  void getOthers(OthersList& L) const;
+
   static bool implementsClass(Kind K) { return K == Kind::Root; }
 
  private:
@@ -295,6 +298,110 @@ class IntSeqCountNode : public IntCountNode {
 
  protected:
   void describeValues(FILE* Out) const OVERRIDE;
+};
+
+// Defines a visitor for a (root-based) trie.
+class CountNodeVisitor {
+  CountNodeVisitor() = delete;
+  CountNodeVisitor(const CountNodeVisitor&) = delete;
+  CountNodeVisitor& operator=(const CountNodeVisitor&) = delete;
+
+ public:
+  class Frame;
+  typedef std::shared_ptr<Frame> FramePtr;
+
+  enum class State { Enter, Visiting, Exit };
+  static const char* getName(State St);
+
+  class Frame {
+    Frame() = delete;
+    Frame(const Frame&) = delete;
+    Frame& operator=(const Frame&) = delete;
+    friend class CountNodeVisitor;
+
+   public:
+    // Create frame referring to root of visitor.
+    Frame(CountNodeVisitor& Visitor, size_t FirstKid, size_t LastKid)
+        : Visitor(Visitor),
+          FirstKid(FirstKid),
+          LastKid(LastKid),
+          CurKid(FirstKid),
+          CurState(State::Enter) {}
+    // Create frame referring to Nd of visitor.
+    Frame(CountNodeVisitor& Visitor,
+          CountNode::IntPtr Nd,
+          size_t FirstKid,
+          size_t LastKid)
+        : Visitor(Visitor),
+          FirstKid(FirstKid),
+          LastKid(LastKid),
+          CurKid(FirstKid),
+          CurState(State::Enter),
+          Nd(Nd) {}
+
+    virtual ~Frame();
+
+    bool isRootFrame() const { return !bool(Nd); }
+    CountNode::RootPtr getRoot() const { return Visitor.getRoot(); }
+
+    bool isIntNodeFrame() const { return bool(Nd); }
+    CountNode::IntPtr getIntNode() const {
+      assert(isIntNodeFrame());
+      return Nd;
+    }
+
+    // Returns the node being visited.
+    CountNode::WithSuccsPtr getNode() const {
+      if (isIntNodeFrame())
+        return Nd;
+      return getRoot();
+    }
+
+    // The following is for debugging.
+    void describe(FILE* Out) const;
+    virtual void describePrefix(FILE* Out) const;
+    virtual void describeSuffix(FILE* Out) const;
+
+   protected:
+    CountNodeVisitor& Visitor;
+    size_t FirstKid;
+    size_t LastKid;
+    size_t CurKid;
+    State CurState;
+    CountNode::IntPtr Nd;
+  };
+
+  CountNodeVisitor(CountNode::RootPtr Root);
+  ~CountNodeVisitor() {}
+
+  void walk();
+
+  CountNode::RootPtr getRoot() const { return Root; }
+
+  void describe(FILE* Out) const;
+
+ protected:
+  CountNode::RootPtr Root;
+  std::vector<CountNode::IntPtr> ToVisit;
+  std::vector<FramePtr> Stack;
+
+  virtual FramePtr getFrame(size_t FirstKid, size_t LastKid);
+  virtual FramePtr getFrame(CountNode::IntPtr Nd,
+                            size_t FirstKid,
+                            size_t LastKid);
+
+  void walkOther();
+
+  void callRoot();
+  void callNode(CountNode::IntPtr Nd);
+
+  virtual void visit(FramePtr Frame);
+  // Returns any values to the calling frame (i.e. the topmost frame
+  // in the visitor.
+  virtual void visitReturn(FramePtr Frame);
+
+  // Visits non-root/int count nodes.
+  virtual void visitOther(CountNode::Ptr Nd);
 };
 
 }  // end of namespace intcomp
